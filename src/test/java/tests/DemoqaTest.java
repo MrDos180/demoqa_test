ddfc0be12eede26a10962a9432ef7ec5c760f5a5
MrDos180 <ilya180@yandex.ru>
2022-11-19 19:00:13 +0300

    add variables

--- src/test/java/tests/DemoqaTest.java
+++ src/test/java/tests/DemoqaTest.java
@@ -7,33 +7,48 @@ import pages.RegistrationPage;
 public class DemoqaTest extends TestBase {
     RegistrationPage registrationPage = new RegistrationPage();
 
+    String firstName = "Ivan";
+    String lastName = "Drago";
+    String email = "Drago@gmail.com";
+    String gender = "Male";
+    String number = "8505555555";
+    String dayBirth = "29";
+    String monthBirth = "February";
+    String yearBirth = "1988";
+    String subject = "Math";
+    String hobbie = "Sports";
+    String pictureDirectory = "src/test/resources/ivan.jpeg";
+    String adress = "Moscow, lenina st 25";
+    String state = "Uttar Pradesh";
+    String city = "Agra";
+
     @Test
     void fillFormTest() {
         registrationPage.openPage().
-                setFirstName("Ivan").
-                setLastName("Drago").
-                setEmail("Drago@gmail.com").
-                setGender("Male").
-                setUserNumber("8505555555").
-                setBDate("29", "February", "1988").
-                setSubject("Math").
-                setHobbies("Sports").
-                uploadPicture("src/test/resources/ivan.jpeg").
-                setAdress("Moscow, lenina st 25").
-                setState("Uttar Pradesh").
-                setCity("Agra").
+                setFirstName(firstName).
+                setLastName(lastName).
+                setEmail(email).
+                setGender(gender).
+                setUserNumber(number).
+                setBDate(dayBirth, monthBirth, yearBirth).
+                setSubject(subject).
+                setHobbies(hobbie).
+                uploadPicture(pictureDirectory).
+                setAdress(adress).
+                setState(state).
+                setCity(city).
                 clickSubmit().
                 verifyResultsModalAppears().
-                verifyResult("Student Name", "Ivan Drago").
-                verifyResult("Student Email", "Drago@gmail.com").
-                verifyResult("Gender", "Male").
-                verifyResult("Mobile", "8505555555").
-                verifyResult("Date of Birth", "29 February,1988").
-                verifyResult("Subjects", "Maths").
-                verifyResult("Hobbies", "Sports").
-                verifyResult("Picture", "ivan.jpeg").
-                verifyResult("Address", "Moscow, lenina st 25").
-                verifyResult("State and City", "Uttar Pradesh Agra").
+                verifyResult("Student Name", firstName+" "+ lastName).
+                verifyResult("Student Email", email).
+                verifyResult("Gender", gender).
+                verifyResult("Mobile", number).
+                verifyResult("Date of Birth", dayBirth+" "+ monthBirth+","+yearBirth).
+                verifyResult("Subjects", subject).
+                verifyResult("Hobbies", hobbie).
+                verifyResult("Picture", pictureDirectory.replaceAll("src/test/resources/","")).
+                verifyResult("Address", adress).
+                verifyResult("State and City", state+ " "+city).
                 clickClose();
 
 
